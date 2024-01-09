d84a7121d90b99262ea835a752b44a0aefe94ff8
gerardopena <35822482+gerardopena@users.noreply.github.com>
2024-01-09 00:02:33 -0800

    First commit

---
+++ Components/StatusImage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Image, View, StyleSheet} from 'react-native';
+
+const StatusImage = () => {
+    return (
+        <View>
+            
+            <Image
+                source={require('../assets/Clown.png')}
+            />
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    
+})
+
+export default StatusImage;
