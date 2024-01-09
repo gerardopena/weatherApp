1e1517de226f763f675839d896019696904eacf5
Jose <josemanuel21302@gmail.com>
2024-01-09 00:58:50 -0800

    added simple layout for the app

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StatusImage from './Components/StatusImage';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [city, setCity] = useState('City Name')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusImage/>
+      <Text>{city}</Text>
+      <Text>Date</Text>
+      <Text>Temp</Text>
+      {/* <StatusImage/> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4287f5',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, 
 });
